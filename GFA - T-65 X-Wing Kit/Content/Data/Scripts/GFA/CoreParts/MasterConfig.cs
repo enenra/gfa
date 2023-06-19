39983e43946b13ee52531846f770a992f6240890
enenra <enenra9@gmail.com>
2023-06-19 12:37:23 +0200

    X-Wing - Progress: Finished cockpit, fixes, started WC setup

---
+++ GFA - T-65 X-Wing Kit/Content/Data/Scripts/GFA/CoreParts/MasterConfig.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            PartDefinitions(GFA_Weapon_XWing_LaserCannon_BottomLeft, GFA_Weapon_XWing_LaserCannon_BottomRight, GFA_Weapon_XWing_LaserCannon_TopRight, GFA_Weapon_XWing_LaserCannon_TopLeft);
+        }
+    }
+}
