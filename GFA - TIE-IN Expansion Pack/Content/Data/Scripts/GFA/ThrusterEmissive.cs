85626bf5f6f1264e27c41eadb9d62874fa35d302
enenra <enenra9@gmail.com>
2024-05-08 22:35:16 +0200

    TIE/in - First Version

---
+++ GFA - TIE-IN Expansion Pack/Content/Data/Scripts/GFA/ThrusterEmissive.cs
@@ -0,0 +1,68 @@
+using VRage.Game.Components;
+using Sandbox.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.ModAPI;
+using VRageMath;
+using Sandbox.Common.ObjectBuilders;
+
+namespace enenra.ThrusterEmissive
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Thrust), false)]
+
+    public class Container : MyGameLogicComponent
+    {
+        IMyThrust m_block;
+
+        private const string EMISSIVE_MATERIAL_NAME = "Emissive";
+        private Color RED = new Color(255, 30, 20);
+        private Color BLACK = new Color(0, 0, 0);
+
+        bool IsWorking => m_block.IsWorking;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            m_block = Entity as IMyThrust;
+
+            var subtype = m_block.BlockDefinition.SubtypeName;
+            if (subtype != "GFA_SG_TIEFighter_Thruster_IN")
+            {
+                m_block = null;
+                return;
+            }
+
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+
+            m_block.IsWorkingChanged += OnIsWorkingChanged;
+            base.Init(objectBuilder);
+        }
+
+        public override void Close()
+        {
+            if (m_block != null)
+            {
+                m_block.IsWorkingChanged -= OnIsWorkingChanged;
+                m_block = null;
+            }
+
+            base.Close();
+        }
+
+        private void OnIsWorkingChanged(VRage.Game.ModAPI.IMyCubeBlock obj)
+        {
+            if (MyAPIGateway.Session == null)
+                return;
+
+            if (MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
+                return;
+
+            if (IsWorking)
+            {
+                m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, RED, 5f);
+            }
+            else 
+            {
+                m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, BLACK, 1f);
+            }
+        }
+    }
+}
