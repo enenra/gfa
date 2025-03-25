1ea1cb9cae39c5bd1783120f9ae20e6e838a0c0c
enenra <enenra9@gmail.com>
2025-03-25 20:11:09 +0100

    Rebel Turrets - Damage Buff for Turret Droid to get it in line with Interior Turret

--- GFA - Rebel Turrets/Content/Data/Scripts/GFA/CoreParts/GFA_Ammo_TurretDroid.cs
+++ GFA - Rebel Turrets/Content/Data/Scripts/GFA/CoreParts/GFA_Ammo_TurretDroid.cs
@@ -46,7 +46,7 @@ namespace Scripts
             TerminalName = "Red Blaster Cartridge", // Optional terminal name for this ammo type, used when picking ammo/cycling consumables.  Safe to have duplicates across different ammo defs.
             HybridRound = true, // Use both a physical ammo magazine and energy per shot.
             EnergyCost = 0.1f, // Scaler for energy per shot (EnergyCost * BaseDamage * (RateOfFire / 3600) * BarrelsPerShot * TrajectilesPerBarrel). Uses EffectStrength instead of BaseDamage if EWAR.
-            BaseDamage = 30f, // Direct damage; one steel plate is worth 100.
+            BaseDamage = 50f, // Direct damage; one steel plate is worth 100.
             Mass = 0.1f, // In kilograms; how much force the impact will apply to the target.
             Health = 0, // How much damage the projectile can take from other projectiles (base of 1 per hit) before dying; 0 disables this and makes the projectile untargetable.
             EnergyMagazineSize = 1000, // For energy weapons, how many shots to fire before reloading.
