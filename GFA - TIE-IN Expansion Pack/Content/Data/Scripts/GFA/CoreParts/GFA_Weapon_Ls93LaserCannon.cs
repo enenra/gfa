614891d4aff689750f697ccdc38f1129654acb04
enenra <enenra9@gmail.com>
2025-03-29 16:25:53 +0100

    TIE/in 1.0b - Targeting options

--- GFA - TIE-IN Expansion Pack/Content/Data/Scripts/GFA/CoreParts/GFA_Weapon_Ls93LaserCannon.cs
+++ GFA - TIE-IN Expansion Pack/Content/Data/Scripts/GFA/CoreParts/GFA_Weapon_Ls93LaserCannon.cs
@@ -37,7 +37,7 @@ namespace Scripts {
             Targeting = new TargetingDef
             {
                 Threats = new[] {
-                    Grids, // Types of threat to engage: Grids, Projectiles, Characters, Meteors, Neutrals, ScanRoid, ScanPlanet, ScanFriendlyCharacter, ScanFriendlyGrid, ScanEnemyCharacter, ScanEnemyGrid, ScanNeutralCharacter, ScanNeutralGrid, ScanUnOwnedGrid, ScanOwnersGrid
+                    Grids, Projectiles, Characters, Meteors, // Types of threat to engage: Grids, Projectiles, Characters, Meteors, Neutrals, ScanRoid, ScanPlanet, ScanFriendlyCharacter, ScanFriendlyGrid, ScanEnemyCharacter, ScanEnemyGrid, ScanNeutralCharacter, ScanNeutralGrid, ScanUnOwnedGrid, ScanOwnersGrid
                 },
                 SubSystems = new[] {
                     Thrust, Utility, Offense, Power, Production, Any, // Subsystem targeting priority: Offense, Utility, Power, Production, Thrust, Jumping, Steering, Any
--- GFA - TIE-IN Expansion Pack/Content/patch_notes.md
+++ GFA - TIE-IN Expansion Pack/Content/patch_notes.md
@@ -1,4 +1,5 @@
 [h1]Patch 1.0b[/h1]
 [list]
 [*]Increase bolt travel distance but kept lock distance the same.
+[*]Added targeting for projectiles, characters, meteors.
 [/list]
